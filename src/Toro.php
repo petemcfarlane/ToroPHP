53cc0f91a88e9eca239c3b2a8e47bb97a22ef28f
Pete <pmcfarlane@workanyware.co.uk>
2014-02-23 15:22:33 +0000

    Pass custom method name in handler using "@"

--- src/Toro.php
+++ src/Toro.php
@@ -42,6 +42,12 @@ class Toro
             }
         }
 
+	    if (strpos($discovered_handler, '@')) {
+		    $position = strpos($discovered_handler, '@');
+		    $request_method = substr($discovered_handler, $position + 1);
+		    $discovered_handler = substr($discovered_handler, 0, $position);
+	    }
+
         $result = null;
 
         $handler_instance = null;
